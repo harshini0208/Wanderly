45c0bcc2689c8b7bf0945a64d746b2b2acfbcdf3
Harshini S <harshinis@Harshinis-MacBook-Pro.local>
2025-11-02 00:56:42 +0530

    modify vite.config.js for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Wanderly-1/',
+  base: './',
   server: {
     port: 3000,
   },
